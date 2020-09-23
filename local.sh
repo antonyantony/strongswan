32b871fbc643291a15a8e9303f7a2c6f060b99f0
Antony <antony.antony@secunet.com>
2020-09-23 16:09:04 +0200

    local.sh

---
+++ local.sh
@@ -0,0 +1,9 @@
+./configure \
+    --enable-swanctl \
+    --enable-tss-trousers \
+    --enable-systemd \
+    --enable-openssl \
+    --enable-aesni \
+    --enable-ccm \
+    --enable-gcm \
+    --disable-tss-trousers
