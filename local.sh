70059172081b27030abd2aaa8b93c983423f644b
Antony Antony <antony@phenome.org>
2021-07-06 06:06:51 +0000

    local.sh --enable-cmd

--- local.sh
+++ local.sh
@@ -1,5 +1,4 @@
 export CFLAGS="${CFLAGS} -g -O0 -Wall -Wno-format -Wno-format-security -Wno-pointer-sign -Wfatal-errors -Werror"
 
 ./configure --libdir=/usr/lib --libexecdir=/usr/lib --sysconfdir=/etc/ \
-	--enable-openssl \
-	--enable-bypass-lan
+	--enable-openssl --disable-gmp --enable-cmd
