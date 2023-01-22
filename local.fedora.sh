a4bf2f64162be9e7a78d8c01b3a3be34e5a4ee1f
Antony Antony <antony.antony@secunet.com>
2023-01-22 13:26:11 +0100

    local compile files

---
+++ local.fedora.sh
@@ -0,0 +1,12 @@
+export prefix="/usr"
+export CFLAGS="-g -O0 -Wall -Wno-format -Wno-format-security -Wno-pointer-sign -Wfatal-errors -Werror"
+./configure --prefix=${prefix} \
+    --with-ipsec-script=strongswan \
+    --sysconfdir=/etc/strongswan \
+    --with-ipsecdir=${prefix}/libexec/strongswan \
+    --bindir=${libexecdir}/strongswan \
+    --with-ipseclibdir=${prefix}/lib/strongswan \
+    --with-piddir=/run/strongswan \
+    --enable-systemd \
+    --enable-openssl \
+	--enable-bypass-lan
