9df36ab304a17bb78504d1a9919546a75851c9f8
Antony Antony <antony.antony@secunet.com>
2025-10-18 16:45:24 +0200

    local.debian.sh local.fedora.sh

---
+++ local.debian.sh
@@ -0,0 +1,7 @@
+./configure --build=x86_64-linux-gnu --prefix=/usr --includedir=\${prefix}/include \
+	--mandir=\${prefix}/share/man --infodir=\${prefix}/share/info --sysconfdir=/etc \
+	--localstatedir=/var --disable-silent-rules --libdir=\${prefix}/lib/x86_64-linux-gnu \
+	--libexecdir=\${prefix}/lib/x86_64-linux-gnu --disable-maintainer-mode \
+	--disable-dependency-tracking --libdir=/usr/lib --libexecdir=/usr/lib \
+	--sysconfdir=/etc/ --enable-openssl --disable-gmp --enable-cmd --disable-blowfish \
+	--disable-des --enable-rdrand --with-piddir=/run/strongswan
