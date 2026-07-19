e8e98cbabfcfb47fe0a3b2c336a313d0cb3419b9
Antony Antony <antony.antony@secunet.com>
2026-07-19 14:05:11 +0200

    backup local.debian.sh

--- local.debian.sh
+++ local.debian.sh
@@ -1,7 +1,20 @@
-./configure --build=x86_64-linux-gnu --prefix=/usr --includedir=\${prefix}/include \
-	--mandir=\${prefix}/share/man --infodir=\${prefix}/share/info --sysconfdir=/etc \
-	--localstatedir=/var --disable-silent-rules --libdir=\${prefix}/lib/x86_64-linux-gnu \
-	--libexecdir=\${prefix}/lib/x86_64-linux-gnu --disable-maintainer-mode \
-	--disable-dependency-tracking --libdir=/usr/lib --libexecdir=/usr/lib \
-	--sysconfdir=/etc/ --enable-openssl --disable-gmp --enable-cmd --disable-blowfish \
-	--disable-des --enable-rdrand --with-piddir=/run/strongswan
+#!/bin/sh
+DIR=$(cd "$(dirname "$0")" && pwd)
+"$DIR/configure" --build=x86_64-linux-gnu --prefix=/usr \
+      --includedir=\${prefix}/include \
+      --mandir=\${prefix}/share/man \
+      --infodir=\${prefix}/share/info \
+      --sysconfdir=/etc \
+      --localstatedir=/var \
+      --disable-silent-rules \
+      --libdir=/usr/lib \
+      --libexecdir=/usr/lib \
+      --disable-maintainer-mode \
+      --disable-dependency-tracking \
+      --enable-openssl \
+      --disable-gmp \
+      --enable-cmd \
+      --disable-blowfish \
+      --disable-des \
+      --enable-rdrand \
+      --with-piddir=/run/strongswan
